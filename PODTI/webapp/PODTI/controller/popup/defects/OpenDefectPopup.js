cd2630f2f95c2c247be8735f688b2f0b1979f7c4
Andrea Mancini <andreamancini@kpmg.it>
2026-05-08 14:37:39 +0000

    sap invio supervisore difetti wn diretto

--- PODTI/webapp/PODTI/controller/popup/defects/OpenDefectPopup.js
+++ PODTI/webapp/PODTI/controller/popup/defects/OpenDefectPopup.js
@@ -971,7 +971,7 @@ sap.ui.define([
                 "prodOrder": prodOrder,
                 "descript": defect.defectNote,
                 "dCodegrp": defect.codeGroup,
-                "dCode": defect.defectType,
+                "dCode": defect.sap_code,
                 "assembly": defect.assembly,
                 "quantDefects": "" + defect.numDefect,
                 "partner": defect.responsible,
